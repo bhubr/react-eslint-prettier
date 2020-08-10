c5b25070f48cd22479487ed052ca5ee3fdaf5496
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-08-10 14:29:07 +0200

    Test recent JavaScript constructs in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { Component } from 'react'
 import './App.css'
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  handleClick = () => this.setState(
+    ({ count }) => ({ count: count + 1 })
   )
+
+  render () {
+    const { count } = this.state
+    return (
+      <div className="App">
+        <button
+          type="button"
+          onClick={this.handleClick}
+        >
+          +1
+        </button>
+        <p>Count: {count}</p>
+      </div>
+    )
+  }
 }
 
 export default App
